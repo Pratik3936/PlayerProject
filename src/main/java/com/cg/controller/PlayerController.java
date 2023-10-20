d38d04c0ba44f730f1936566a36f2f9d5ca3bda4
pratik3936 <pmane03936@gmail.com>
2023-10-20 10:56:10 +0530

    Controller class commit

---
+++ src/main/java/com/cg/controller/PlayerController.java
@@ -0,0 +1,33 @@
+package com.cg.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.entity.Player;
+import com.cg.service.PlayerService;
+
+@RestController
+public class PlayerController {
+	
+	@Autowired
+	PlayerService service;
+	
+	@PostMapping("/add")
+	public String addPlayer(Player player) {
+		
+		service.addPlayer(player);
+		return "Player Added";
+		
+	}
+	
+	@GetMapping("/getall")
+	public List<Player> getDetails(){
+		return service.getAll();
+		
+	}
+
+}
